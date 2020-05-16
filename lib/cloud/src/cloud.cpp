e082eac80c492f21deeb1a667351935a13134bd9
marwasha <marwasha@umich.edu>
2020-05-16 10:19:35 -0400

    trying to get subscriber function to work

--- lib/cloud/src/cloud.cpp
+++ lib/cloud/src/cloud.cpp
@@ -13,6 +13,24 @@ void pullTempOut(const char *event, const char *data) {
   outsideTemp = atof(data);
 }
 */
+
+OWMWH::OWMWH() {
+  Particle.subscribe("hook-response/outsideTemp", OWMWH::readOT, MY_DEVICES);
+}
+
+void OWMWH::readOT(const char *event, const char *data) {
+  outsideTemp = atof(data);
+}
+
+void OWMWH::requestOT() {
+  Particle.publish("outsideTemp", "", PRIVATE);
+}
+
+float OWMWH::getOutsideTemp() {
+  requestOT();
+  return outsideTemp;
+}
+
 void pushTemp(float temp) {
   String Tempurature = String(temp);
   Particle.publish("Tempurature", Tempurature, PRIVATE);
--- lib/cloud/src/cloud.h
+++ lib/cloud/src/cloud.h
@@ -1,7 +1,20 @@
-float outsideTemp;
+//float outsideTemp = 0;
 /*
 void requestOutsideTemp(void);
 void pullTempOutSetup(void);
 void pullTempOut(const char *event, const char *data);
 */
+
+
+class OWMWH {
+  public:
+    OWMWH();
+    float getOutsideTemp();
+
+  private:
+    float outsideTemp;
+    void requestOT();
+    void readOT(const char *event, const char *data);
+};
+
 void pushTemp(float temp);
